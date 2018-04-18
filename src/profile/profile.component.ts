88be5826617b369968848cf7dd06b2aa70a9a28e
Nisha <nisha@blinfosoft.com>
2018-04-18 15:37:37 +0530

    first commit

---
+++ src/profile/profile.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'settings',
+  template: ` <style>
+    .innerContent{    padding: 50px;
+      border: 1px solid;
+      margin: 50px 0px 0px 50px;}</style>
+  <div class="innerContent">
+    <div>
+      <h2>Hi! I am Setting</h2>
+      <p>hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello </p>
+      <p>hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello </p>
+      <p>hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello </p>
+      <p>hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello </p>
+    </div>
+  `
+})
+export class SettingsComponent {}
